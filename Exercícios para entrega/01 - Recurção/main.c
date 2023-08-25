a7e9357b6b27d5a4edd494f14f98395cde16b3e2
rolisola <vitor.rolisola@gmail.com>
2023-08-25 15:08:32 -0300

    Update Recurção/main.c

--- Exercícios para entrega/01 - Recurção/main.c
+++ Exercícios para entrega/01 - Recurção/main.c
@@ -6,20 +6,20 @@
 
 
 
-//funcao para identificar numero primo de forma recursiva
+//Funcao para identificar numero primo de forma recursiva
 int primo_recursivo(unsigned int valor, unsigned int divisor){
-    if(divisor <= 1){
+    if(divisor*divisor > valor){
         return 1; //Chegou ao fim da recursao sem encontrar divisores
     }
-    if(valor % divisor == 0){
+    if( (valor%divisor == 0) && (divisor != 1) ){
         return 0; //Encontrou um divisor, nao e primo
     }
-    return primo_recursivo(valor, divisor - 1);
+    return primo_recursivo(valor, divisor + 2); //Testa somente numeros impares
 }
 
-//funcao para identificar numero primo de forma iterativa
+//Funcao para identificar numero primo de forma iterativa
 int primo_iterativo(unsigned int valor){
-    for(unsigned  int i = 3 ; i*i <= valor ; i = i+2){
+    for(unsigned  int i = 3 ; i*i <= valor ; i += 2){ //Testa somente numeros impares
         if(valor % i == 0){
             return 0; //Encontrou um divisor, nao e primo
         }
@@ -34,11 +34,12 @@ int main(){
         printf("Digite um numero inteiro positivo, 0 para sair:\n");
         scanf("%u", &valor);
 
-        if(valor == 2){
+        if(valor == 2){ //Unico numero primo par
             printf("%u e numero primo\n", valor);
-        } else if(((valor % 2 == 0) && (valor != 2)) || (valor == 1)){
+        } else if( ( (valor % 2 == 0) && (valor != 2) ) || (valor == 1) ){ //Verifica se o numero e par
             printf("%u nao e numero primo\n", valor);
         }else{
+
             /*modo iterativo*/
             if(primo_iterativo(valor)){
                 printf("%u e um numero primo (iterativo)\n", valor);
@@ -47,12 +48,13 @@ int main(){
             }
 
             /*modo recursivo*/
-            if(primo_recursivo(valor, sqrt(valor))){
+            if(primo_recursivo(valor, 1)){
                 printf("%u e um numero primo (recursivo)\n", valor);
             }else{
                 printf("%u nao e um numero primo (recursivo)\n", valor);
             }
         }
+
     }while(valor != 0);
 
     return 0;
